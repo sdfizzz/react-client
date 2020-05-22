4518d6383dd3730891470a010541bb30efb73608
Alexey Chupin <sdfizzz@gmail.com>
2020-05-22 10:47:22 +0300

    refact styles header menu

--- src/js/components/header/HeaderMenu.js
+++ src/js/components/header/HeaderMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -10,28 +10,11 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
 import SendIcon from '@material-ui/icons/Send';
 
-const StyledMenu = withStyles({
+const useStyles = makeStyles(theme => ({
     paper: {
         border: '1px solid #d3d4d5'
-    }
-})(props => (
-    <Menu
-        elevation={0}
-        getContentAnchorEl={null}
-        anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center'
-        }}
-        transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center'
-        }}
-        {...props}
-    />
-));
-
-const StyledMenuItem = withStyles(theme => ({
-    root: {
+    },
+    item: {
         '&:focus': {
             backgroundColor: theme.palette.primary.main,
             '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
@@ -39,9 +22,9 @@ const StyledMenuItem = withStyles(theme => ({
             }
         }
     }
-}))(MenuItem);
+}));
 
-const HeaderMenu = ({ classes }) => {
+const HeaderMenu = props => {
     const [anchorEl, setAnchorEl] = React.useState(null);
 
     const handleClick = event => {
@@ -52,38 +35,49 @@ const HeaderMenu = ({ classes }) => {
         setAnchorEl(null);
     };
 
+    const classes = useStyles(props);
+
     return (
         <div>
-            <IconButton edge="start" className={classes} color="inherit" aria-label="menu" onClick={handleClick}>
+            <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleClick}>
                 <MenuIcon />
             </IconButton>
-
-            <StyledMenu
+            <Menu
+                elevation={0}
+                getContentAnchorEl={null}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center'
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center'
+                }}
                 id="customized-menu"
                 anchorEl={anchorEl}
                 keepMounted
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
             >
-                <StyledMenuItem>
+                <MenuItem className={classes.item}>
                     <ListItemIcon>
                         <SendIcon fontSize="small" />
                     </ListItemIcon>
                     <ListItemText primary="Sent mail" />
-                </StyledMenuItem>
-                <StyledMenuItem>
+                </MenuItem>
+                <MenuItem className={classes.item}>
                     <ListItemIcon>
                         <DraftsIcon fontSize="small" />
                     </ListItemIcon>
                     <ListItemText primary="Drafts" />
-                </StyledMenuItem>
-                <StyledMenuItem>
+                </MenuItem>
+                <MenuItem className={classes.item}>
                     <ListItemIcon>
                         <InboxIcon fontSize="small" />
                     </ListItemIcon>
                     <ListItemText primary="Inbox" />
-                </StyledMenuItem>
-            </StyledMenu>
+                </MenuItem>
+            </Menu>
         </div>
     );
 };
